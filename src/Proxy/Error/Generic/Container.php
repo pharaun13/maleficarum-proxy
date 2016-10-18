7c9cd4510be779c6a0ded31075f83d2b5dc432fc
Adam Kowalski <adam.kowalski@miinto.com>
2016-10-18 14:46:16 +0200

    Initial commit

---
+++ src/Proxy/Error/Generic/Container.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This class provides a generic error container trait usage.
+ */
+
+namespace Maleficarum\Proxy\Error\Generic;
+
+class Container
+{
+    /**
+     * Use \Maleficarum\Proxy\Error\Container trait functionality.
+     *
+     * @trait
+     */
+    use \Maleficarum\Proxy\Error\Container;
+}
